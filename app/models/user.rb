4f2ee3cc76285fb2612a0c63657666c8fbc5cd4a
Rustamon <russ.himelein@yahoo.com>
2015-11-21 19:41:49 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   
   before_save   :downcase_email
@@ -63,7 +64,9 @@ class User < ActiveRecord::Base
      def password_reset_expired?
        reset_sent_at < 2.hours.ago
      end
-  
+     def feed
+         Micropost.where("user_id = ?", id)
+       end
   private
 
       # Converts email to all lower-case.
